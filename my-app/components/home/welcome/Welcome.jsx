a818a74d00465040a686d5b9142d7878c80329f0
karthik-reddy <karthikb10005@gmail.com>
2024-07-13 19:04:23 +0530

    before adding the search bar

--- my-app/components/home/welcome/Welcome.jsx
+++ my-app/components/home/welcome/Welcome.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import {icon, SIZES} from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Mr</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
     </View>
   )
 }
